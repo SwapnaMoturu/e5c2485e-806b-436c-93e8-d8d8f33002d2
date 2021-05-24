3d257060fdaf73686ebfbec644146348c0206cef
SwapnaMoturu <mrsharkut@gmail.com>
2021-05-24 23:05:33 +1000

    Add project files.

---
+++ LIS_Api/Controllers/LISController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace LIS_Api.Controllers
+{
+    public class Response
+    {
+        public string Output { get; set; }
+    }
+    public class LISController : Controller
+    {
+        private readonly ILogger<LISController> _logger;
+
+        public LISController(ILogger<LISController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public string GetLIS([FromBody] string inputValues)
+        {
+            Response resp = new Response();
+            LIS objLIS = new LIS();
+            Regex rgx = new Regex(@"^[\d\s]+$");
+            if (!String.IsNullOrEmpty(inputValues) && rgx.IsMatch(inputValues))
+            {
+                int[] line = Array.ConvertAll<string, int>(inputValues.ToString().Split(' '), Convert.ToInt32);
+                if (line != null)
+                {
+                    objLIS.Solve(line);
+                    if (objLIS.output != null)
+                        resp.Output = String.Join(" ", objLIS.output);
+                }
+            }
+            return resp.Output;
+        }
+    }
+}
---
+++ LIS_Api/ILIS.cs
@@ -0,0 +1,10 @@
+﻿namespace LIS_Api
+{
+    public interface ILIS
+    {
+        string input { get; set; }
+        string output { get; set; }
+
+        void Solve(int[] arr);
+    }
+}
